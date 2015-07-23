967d93890439293a05fb3f07f71fa9e3ef53cee9
Tomasz Gorochowik <tgorochowik@antmicro.com>
2015-07-23 09:55:51 +0200

    bif2bin: add partition data related structs
    This commits also adds partition data related defines.

--- bif2bin/bootrom.h
+++ bif2bin/bootrom.h
@@ -33,6 +33,67 @@ typedef struct bootrom_hdr_t {
   };
 } bootrom_hdr_t;
 
+/* BootROM image header table based on ug821 */
+typedef struct bootrom_img_hdr_tab_t{
+  uint32_t version;
+  uint32_t hdrs_count;
+  uint32_t part_hdr_off; /* word offset to the partition header */
+  uint32_t img_hdr_off; /* word offset to first image header */
+  uint32_t auth_hdr_off; /* word offset to header authentication */
+  uint32_t padding;
+} bootrom_img_hdr_tab_t;
+
+/* BootROM partition header based on ug821 */
+/* All offsets are relative to the start of the boot image */
+typedef struct bootrom_partition_hdr_t{
+  uint32_t pd_word_len; /* encrypted partiton data length */
+  uint32_t ed_word_len; /* unecrypted data length */
+  uint32_t total_word_len; /* total encrypted,padding,expansion, auth length */
+
+  uint32_t dest_load_addr; /* RAM addr where the part will be loaded */
+  uint32_t dest_exec_addr;
+
+  uint32_t data_off;
+  uint32_t attributes;
+  uint32_t section_count;
+
+  uint32_t checksum_off;
+  uint32_t img_hdr_off;
+  uint32_t cert_off;
+
+  uint32_t reserved[4]; /* set to 0 */
+
+  uint32_t checksum;
+} bootrom_partition_hdr_t;
+
+/* BootROM image header based on ug821 */
+typedef struct bootrom_img_hdr_t{
+  uint32_t next_img_off; /* 0 if last */
+  uint32_t part_hdr_off;
+  uint32_t part_count; /* set to 0 */
+  uint32_t name_len;
+  uint32_t name;
+  uint32_t terminator; /* set to 0 */
+} bootrom_img_hdr_t;
+
+/* attributes of the bootrom partition header */
+#define BOOTROM_PART_ATTR_OWNER_OFF      16
+#define BOOTROM_PART_ATTR_OWNER_MASK     (3 << BOOTROM_PART_ATTR_OWNER_OFF)
+#define BOOTROM_PART_ATTR_OWNER_FSBL     0
+#define BOOTROM_PART_ATTR_OWNER_UBOOT    1
+
+#define BOOTROM_PART_ATTR_RSA_USED_OFF   15
+#define BOOTROM_PART_ATTR_RSA_USED_MASK  (1 << BOOTROM_PATR_ATTR_RSA_USED_OFF)
+#define BOOTROM_PART_ATTR_RSA_USED       1
+#define BOOTROM_PART_ATTR_RSA_NOT_USED   0
+
+#define BOOTROM_PART_ATTR_DEST_DEV_OFF   4
+#define BOOTROM_PART_ATTR_DEST_DEV_MASK  (7 << BOOTRM_PART_ATTR_DEST_DEV_OFF)
+#define BOOTROM_PART_ATTR_DEST_DEV_NONE  0
+#define BOOTROM_PART_ATTR_DEST_DEV_PS    1
+#define BOOTROM_PART_ATTR_DEST_DEV_PL    2
+#define BOOTROM_PART_ATTR_DEST_DEV_INT   3
+
 /* values taken from boot.bin generated with bootgen */
 #define BOOTROM_INT_TABLE_DEFAULT 0xEAFFFFFE
 #define BOOTROM_USER_0            0x01010000 /* probably not needed */
@@ -47,6 +108,8 @@ typedef struct bootrom_hdr_t {
 #define BOOTROM_MIN_SRC_OFFSET    0x000008C0
 #define BOOTROM_RESERVED_0        0x00000000 /* MUST be set to 0 */
 #define BOOTROM_RESERVED_1        0x00000000 /* MUST be set to 0 */
+
+#define BOOTROM_IMG_VERSION       0x01020000
 
 /* Other file specific files */
 #define FILE_MAGIC_ELF            0x464C457F
